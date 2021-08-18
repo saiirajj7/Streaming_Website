428264a32f569dba893d67fd159e782fcb203012
saiirajj7 <sairajdhonnar01@gmail.com>
2021-08-18 20:57:24 +0530

    TE Project

---
+++ Includes/config.php
@@ -0,0 +1,14 @@
+
+<?php
+ob_start(); // Turns on output buffering
+session_start();
+date_default_timezone_set("Asia/Calcutta");
+//It will try to connect to database
+try {
+    $con = new PDO("mysql:dbname=fimcut;host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch (PDOException $e) {
+    exit("Connection failed: " . $e->getMessage());
+}
+?>
