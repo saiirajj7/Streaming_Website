2c91106276c8c8350e202a3ac250bf8d5b6e505b
Kewal Patel <72654260+kewalpatel1@users.noreply.github.com>
2021-10-03 22:09:30 +0530

    uploaders

--- Includes/config.php
+++ Includes/config.php
@@ -5,7 +5,7 @@ session_start();
 date_default_timezone_set("Asia/Calcutta");
 //It will try to connect to database
 try {
-    $con = new PDO("mysql:dbname=fimcut;host=localhost", "root", "");
+    $con = new PDO("mysql:dbname=streaming;host=localhost", "root", "");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e) {
